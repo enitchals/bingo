52930f0daeb1b4905ad7a5b342a0e87026193372
enitchals <ellen.nitchals@gmail.com>
2023-01-21 00:28:06 -0800

    un-bork

--- makeTemplate.js
+++ makeTemplate.js
@@ -1,61 +1,54 @@
-const titleInput = document.querySelector('#prompt-input');
-const promptInput = document.querySelector('#prompt-input');
-const templateList = document.querySelector('#existingTemplates');
-const cardPreviewElement = document.querySelector('#preview');
-const list = document.querySelector('#prompt-list');
-const makeTemplateButton = document.querySelector('#makeTemplate');
+// export const makeButton = (text, callback, buttonClass) => {
+//   const button = document.createElement('button');
+//   button.innerText = text;
+//   button.addEventListener('click', callback);
+//   button.className = buttonClass;
+//   return button;
+// }
 
-export const makeButton = (text, callback, buttonClass) => {
-  const button = document.createElement('button');
-  button.innerText = text;
-  button.addEventListener('click', callback);
-  button.className = buttonClass;
-  return button;
-}
+// templateList.append(...promptOptions.map(option => makeButton(option.buttonText, () => pickTemplate(option.array))))
 
-templateList.append(...promptOptions.map(option => makeButton(option.buttonText, () => pickTemplate(option.array))))
+// let prompts = [];
 
-let prompts = [];
+// const renderList = () => {
+//   list.replaceChildren(...prompts.map(renderPrompt));
+// }
 
-const renderList = () => {
-  list.replaceChildren(...prompts.map(renderPrompt));
-}
+// const removeItemFromPrompts = (item) => {
+//   prompts = prompts.filter(p => p !== item);
+//   renderList();
+// }
 
-const removeItemFromPrompts = (item) => {
-  prompts = prompts.filter(p => p !== item);
-  renderList();
-}
+// const renderPrompt = (prompt) => {
+//   const promptEl = document.createElement('section');
+//   promptEl.className = 'prompt';
+//   const promptText = document.createElement('li');
+//   promptText.innerText = prompt;
+//   const deleteButton = makeButton('X', () => removeItemFromPrompts(prompt), 'deleteButton')
+//   promptEl.append(deleteButton, promptText);
+//   return promptEl;
+// }
 
-const renderPrompt = (prompt) => {
-  const promptEl = document.createElement('section');
-  promptEl.className = 'prompt';
-  const promptText = document.createElement('li');
-  promptText.innerText = prompt;
-  const deleteButton = makeButton('X', () => removeItemFromPrompts(prompt), 'deleteButton')
-  promptEl.append(deleteButton, promptText);
-  return promptEl;
-}
+// function addPrompt(){
+//   prompts.push(promptInput.value);
+//   promptInput.value = '';
+//   renderList();
+// }
 
-function addPrompt(){
-  prompts.push(promptInput.value);
-  promptInput.value = '';
-  renderList();
-}
+// function pickTemplate(template) {
+//   prompts = template;
+//   renderList();
+// }
 
-function pickTemplate(template) {
-  prompts = template;
-  renderList();
-}
+// function makeTemplate() {
+//   const template = new CardTemplate(titleInput.value, null, prompts);
+//   titleInput.value = '';
+//   makeTemplateButton.innerText = 'New Card';
+//   return template;
+// }
 
-function makeTemplate() {
-  const template = new CardTemplate(titleInput.value, null, prompts);
-  titleInput.value = '';
-  makeTemplateButton.innerText = 'New Card';
-  return template;
-}
-
-function makeCard(){
-  const temp = makeTemplate();
-  const card = temp.generateNewCard();
-  cardPreviewElement.replaceChildren(card.render());
-}
+// function makeCard(){
+//   const temp = makeTemplate();
+//   const card = temp.generateNewCard();
+//   cardPreviewElement.replaceChildren(card.render());
+// }
