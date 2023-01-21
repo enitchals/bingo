11e5cd7042fdcc3ccb808da2891f3d0fe9476459
enitchals <ellen.nitchals@gmail.com>
2023-01-20 22:47:04 -0800

    commit

--- index.js
+++ index.js
@@ -14,4 +14,5 @@ const numberBingo = new CardTemplate('bingo!', 'free', nums);
 const card1 = numberBingo.generateNewCard();
 const card2 = numberBingo.generateNewCard();
 const card3 = numberBingo.generateNewCard();
-cardListElement.append(card1.render(), card2.render(), card3.render())
+// cardListElement.append(card1.render(), card2.render(), card3.render())
+cardListElement.append(card1.render())
